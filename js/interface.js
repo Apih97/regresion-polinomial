b54b36cca03e187a91673e5ecac3d86d2c22a239
Apih97 <apih97@outlook.es>
2018-11-28 17:16:20 -0600

    Update interface.js

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,6 @@
 var order=2, n=9;
 
-loadSample([[0,0,1,2,0,1,2,2,1],[0,2,2,4,4,6,6,2,1],[14,21,11,12,23,23,14,6,11]]); //carga valores de ejemplo en la matriz
+loadSample([[0,1,2,3,4,5],[2.1,7.7,13.6,27.2,40.9,61.1]]);
 
 function crearTabla(rows, cols) {
   var esp = document.querySelector(".datainput");
@@ -10,11 +10,11 @@ function crearTabla(rows, cols) {
 
   for (var i = 0; i <= rows; i++) {
     var fila = document.createElement("tr");
-    for (var j = 0; j <= cols; j++) {
+    for (var j = 0; j <= 2; j++) {
       var celda = document.createElement("td");
       if (i==0) {
         if (j>0)
-          celda.innerHTML = (j==cols ? "Y":"X<sub>" + j +"</sub>");
+          celda.innerHTML = (j==2 ? "Y":"X" );
       }
       else
         celda.innerHTML = (j==0 ? "<sub>" + i + "</sub>": "<input>");
@@ -26,12 +26,12 @@ function crearTabla(rows, cols) {
   esp.append(tabla);
 }
 
-document.querySelector("#order").addEventListener("input", function(e) { //agrega manejador de evento cuando cambia el numero en recuadro
+document.querySelector("#order").addEventListener("input", function(e) {
   order = +e.target.value;
   crearTabla(n,order+1);
 });
 
-document.querySelector("#n").addEventListener("input", function(e) {  //agrega manejador de evento cuando cambia el numero en recuadro
+document.querySelector("#n").addEventListener("input", function(e) {
   n = +e.target.value;
   crearTabla(n,order+1);
 });
@@ -54,15 +54,32 @@ document.querySelector("button").addEventListener("click", function() {
   
   var elementos = document.querySelectorAll("table input");
 
-  for (var i = 0; i < elementos.length; i++) { //leer valores input y los inserta en data ordenadamente
+  for (var i = 0; i < elementos.length; i++) {
     var v = +elementos[i].value;
-    if (i <= order) {
-      data.push([]);  //ingresar en una dimension                            
-      data[i].push(v);  //ingresar en otra dimension
+    if (i <= 1) {
+      data.push([]);
+      data[i].push(v);
     }
     else
-      data[i%(order+1)].push(v);
+      data[i%(2)].push(v);
   }
+
+  var reg = regresion(data,order);
+  
+  var results = document.querySelector("#results");
+  var txt = "<h1>Resultados</h1>Ecuación: " + equation(reg[0]) + "<br>";
+  txt += "Error estándar: " + reg[1].toFixed(3) + "<br>";
+  txt += "R cuadrado: " + reg[2].toFixed(3);
+  results.innerHTML = txt;
+});
+
+function equation(coefs) {
+  return coefs.reduce(function(str,coef,i) {
+    str += (coef > 0 ? "+ ":"- ") + Math.abs(coef.toFixed(5)) + " ";
+    if (i>0) str += "X<sup>" + i + "</sup> ";
+    return str;
+  },"")
+}
 
   var reg = regresion(data);
   
